aa22d3121973648edc9ba67cfca0e8e995f13f2b
Davo Galavotti <davidgalavotti@gmail.com>
2019-01-14 18:24:13 -0500

    Changing Syncope, a little bit.

--- js/app.js
+++ js/app.js
@@ -1,32 +1,41 @@
 /* jshint esnext: true */
 
-const doc = document;
+const doc = document
 
-doc.addEventListener( 'DOMContentLoaded', () => {
+doc.addEventListener('DOMContentLoaded', () => {
+	WebFont.load({
+		custom: { families: ['Graphik:n4', 'Graphik:n5'], urls: ['/css/graphik.css'] },
+		loading: () => {
+			capHeight.setContainer(document.body)
+		},
+		fontactive: capHeight.fontActive(properties => {
+			console.log(properties)
+		})
+	})
+
+	// capHeight.setContainer(document.body)
+
+	// capHeight.calculate({})
+
 	/**
 	 * Main Syncope application.
 	 */
-	new Vue( {
+	new Vue({
 		el: '.app',
 		data: {
 			config: {
 				baseFontSize: 16,
-				baseLineHeight: 1.6,
-				capHeight: 0.68,
+				baseLineHeight: 2,
+				capHeight: 0.72,
 				font: {
-					current: 'Helvetica',
-					options: [
-						{ value: 'Arial', label: 'Arial' },
-						{ value: 'Georgia', label: 'Georgia' },
-						{ value: 'Helvetica', label: 'Helvetica' },
-						{ value: 'Roboto', label: 'Roboto' },
-						{ value: 'Times New Roman', label: 'Times New Roman' },
-					],
-					boldHeaders: true
+					current: 'Graphik',
+					options: [{ value: 'Graphik', label: 'Graphik' }, { value: 'Roboto', label: 'Roboto' }],
+					boldHeaders: false
 				},
 				scale: {
-					current: 1.5,
+					current: 1.2,
 					options: [
+						{ value: 1.125, label: 'Custom (1.125)' },
 						{ value: 1.2, label: 'Minor Third (1.2)' },
 						{ value: 1.25, label: 'Major Third (1.25)' },
 						{ value: 1.333, label: 'Fourth (1.333)' },
@@ -34,13 +43,14 @@ doc.addEventListener( 'DOMContentLoaded', () => {
 						{ value: 1.618, label: 'Fibonacci (1.618)' },
 						{ value: 1.778, label: 'Minor Seventh (1.778)' },
 						{ value: 1.875, label: 'Major Seventh (1.875)' },
-						{ value: 2, label: 'Octave (2.0)' },
+						{ value: 2, label: 'Octave (2.0)' }
 					],
 					factors: {
-						h1: 4,
-						h2: 3,
-						h3: 2,
-						h4: 1,
+						h1: 5,
+						h2: 4,
+						h3: 3,
+						h4: 2,
+						h5: 1,
 						p: 0
 					},
 					headerSpacing: {
@@ -48,17 +58,17 @@ doc.addEventListener( 'DOMContentLoaded', () => {
 						after: 0
 					}
 				},
-				textWidth: 40,
-				outputUnit: 'em',
+				textWidth: 125 / 2,
+				outputUnit: 'px',
 				outputSyntax: 'css',
-				outputVisible: false,
-				showGrid: true,
+				outputVisible: true,
+				showGrid: false
 			},
 
 			output: '',
-			collapse: [ 'basic' ],
-			outputElement: doc.querySelector( '.output code' ),
-			sandboxStylesSheetElement: doc.querySelector( '.sandbox-styles' )
+			collapse: ['basic'],
+			outputElement: doc.querySelector('.output code'),
+			sandboxStylesSheetElement: doc.querySelector('.sandbox-styles')
 		},
 
 		watch: {
@@ -71,47 +81,43 @@ doc.addEventListener( 'DOMContentLoaded', () => {
 		methods: {
 			/**
 			 * Updates the sandbox (preview) with the rhythm styles.
+			 * General principal: multiplier ^ (step / interval)
+
 			 */
 			update() {
-				const cfg = this.config;
-				const rhythms = {};
+				const cfg = this.config
+				const rhythms = {}
 
-				cfg.rhythmUnit = Math.round( cfg.baseFontSize * cfg.baseLineHeight );
+				cfg.rhythmUnit = Math.round(cfg.baseFontSize * cfg.baseLineHeight)
 
-				for ( let i in cfg.scale.factors ) {
-					rhythms[ i ] = new Rhythm( cfg.scale.factors[ i ], cfg );
+				for (let i in cfg.scale.factors) {
+					rhythms[i] = new Rhythm(cfg.scale.factors[i], cfg)
 				}
 
-				this._setSandboxStyles( rhythms );
-				this._setOutput( rhythms );
+				this._setSandboxStyles(rhythms)
+				this._setOutput(rhythms)
 			},
 
-			_setSandboxStyles( rhythms ) {
-				const cfg = this.config;
+			_setSandboxStyles(rhythms) {
+				const cfg = this.config
 
 				let styles = `.sandbox {
-	font: ${ cfg.baseFontSize }px/${ cfg.baseLineHeight } ${ cfg.font.current };
-	padding: ${ cfg.baseLineHeight }em;
-	background: ${ cfg.showGrid ?
-		`linear-gradient(to bottom, rgba( 32,160,255,.35 ) 1px, transparent 1px);`
-		:
-		'transparent;'
-	}
-	max-width: ${ cfg.textWidth }em;
-	background-size: 100% ${ cfg.rhythmUnit }px;
-}`;
+	font: ${cfg.baseFontSize}px/${cfg.baseLineHeight} ${cfg.font.current};
+	padding: ${cfg.baseLineHeight}em;
+	background: ${cfg.showGrid ? `linear-gradient(to bottom, rgba(123,195,255,.35 ) 1px, transparent 1px);` : 'transparent;'}
+	max-width: ${cfg.textWidth}em;
+	background-size: 100% ${cfg.rhythmUnit}px;
+}`
 
-				for ( let i in rhythms ) {
-					styles += `.sandbox ${ i } {${
-	rhythms[ i ].print( 'css', 'px' )
-}}\n\n`;
+				for (let i in rhythms) {
+					styles += `.sandbox ${i} {${rhythms[i].print('css', 'px')}}\n\n`
 				}
 
-				styles += `.sandbox h1, .sandbox h2, .sandbox h3, .sandbox h4 {
-	font-weight: ${ cfg.font.boldHeaders ? 'bold' : 'normal' };
-}`;
+				styles += `.sandbox h1, .sandbox h2, .sandbox h3, .sandbox h4, .sandbox h5 {
+	font-weight: ${cfg.font.boldHeaders ? 'bold' : 'normal'};
+}`
 
-				this.sandboxStylesSheetElement.innerHTML = styles;
+				this.sandboxStylesSheetElement.innerHTML = styles
 			},
 
 			/**
@@ -119,41 +125,36 @@ doc.addEventListener( 'DOMContentLoaded', () => {
 			 *
 			 * @param {Object.<Rhythm>} rhythms
 			 */
-			_setOutput( rhythms ) {
-				const cfg = this.config;
-				let code = `/**
- * Vertical rhythm by Syncope
- * http://nowodzinski.pl/syncope
- */
+			_setOutput(rhythms) {
+				const cfg = this.config
+				let code = `
 
 html, body {
-	font: ${ cfg.baseFontSize }px/${ cfg.baseLineHeight } '${ cfg.font.current }';
+	font: ${cfg.baseFontSize}px/${cfg.baseLineHeight} '${cfg.font.current}';
 }
 
-`;
+`
 
-				if ( !cfg.font.boldHeaders ) {
+				if (!cfg.font.boldHeaders) {
 					code += `h1, h2, h3, h4 {
 	font-weight: normal;
 }
 
-`;
+`
 				}
 
-				if ( cfg.outputSyntax == 'css' ) {
-					for ( let r in rhythms ) {
-						code += `${ r } {${
-	rhythms[ r ].print( cfg.outputSyntax, cfg.outputUnit )
-}}\n\n`;
+				if (cfg.outputSyntax == 'css') {
+					for (let r in rhythms) {
+						code += `${r} {${rhythms[r].print(cfg.outputSyntax, cfg.outputUnit)}}\n\n`
 					}
 				} else {
-					code += '$v-rhythm-levels: (\n';
+					code += '$v-rhythm-levels: (\n'
 
-					for ( let r in rhythms ) {
-						code += `	${ cfg.scale.factors[ r ] }: ( ${ rhythms[ r ].print( cfg.outputSyntax, cfg.outputUnit ) } ),\n`;
+					for (let r in rhythms) {
+						code += `	${cfg.scale.factors[r]}: ( ${rhythms[r].print(cfg.outputSyntax, cfg.outputUnit)} ),\n`
 					}
 
-					code += ');\n';
+					code += ');\n'
 
 					code += `
 /**
@@ -172,11 +173,11 @@ html, body {
 	padding-top: nth( $rhythm, 3 );
 	margin-bottom: nth( $rhythm, 4 );
 }
-`;
+`
 				}
 
-				this.output = hljs.highlight( cfg.outputSyntax, code ).value;
+				this.output = hljs.highlight(cfg.outputSyntax, code).value
 			}
 		}
-	} ).update();
-} );
+	}).update()
+})
